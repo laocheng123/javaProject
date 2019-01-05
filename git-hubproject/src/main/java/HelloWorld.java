e4119bff5d7aa6bef9334875d3dddcf310336a70
chengjiye <2803023227@qq.com>
2019-01-05 16:45:26 +0800

    java项目提交

---
+++ git-hubproject/src/main/java/HelloWorld.java
@@ -0,0 +1,14 @@
+package main.java;
+
+
+/**
+ * @Author: chengjiye
+ * @Date: 2019/1/5 16:32
+ */
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("你好Java！");
+    }
+}
